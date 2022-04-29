bdb3588270767879e916b871871c4ecc811c2ede
DaanSijnja <68638963+DaanSijnja@users.noreply.github.com>
2022-04-29 13:09:27 +0200

    Practium 1A init commit
    Code uit het practicum gekopieerd

--- Practicum 1a/practicum-1a.py
+++ Practicum 1a/practicum-1a.py
@@ -1,7 +1,32 @@
 """
 Practicum Regetechniek van Daan Sijnja (20177747), Jeroen Vogelezang (20093179), Nils Bebelaar (20164882). Wij kunnen echt goed regelen.
 """
-
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib.pylab as plt 
 import control
+
+teller = np.array([2.0]) 
+noemer = np.array([1.0, -3.0]) 
+H = control.tf(teller,noemer)
+print(H)
+
+print("Pole: ",H.pole())
+print("Zero: ",H.zero())
+
+
+K = 2
+
+Sys1 = K*H
+Sys2 = 1
+Hclosed = control.feedback(Sys1,Sys2) 
+
+y,t = control.step_response(H)
+
+
+
+
+plt.plot(t,y)
+plt.ylabel('Stap reponsie')
+plt.xlabel('Tijd [s]')
+plt.legend('Open lus')
+plt.show()
