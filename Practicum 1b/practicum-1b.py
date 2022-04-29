5abc62861fb5fbcb20759a49719f942fb3ca16a4
nilsbebelaar <nils.bebelaar@hotmail.com>
2022-04-29 13:07:32 +0200

    Initial Files

---
+++ Practicum 1b/practicum-1b.py
@@ -0,0 +1,7 @@
+"""
+Practicum Regetechniek van Daan Sijnja (20177747), Jeroen Vogelezang (20093179), Nils Bebelaar (20164882). Wij kunnen echt goed regelen.
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+import control
