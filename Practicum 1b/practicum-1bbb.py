aafa958d91785ec9c59353366284c033e9377e9c
nilsbebelaar <nils.bebelaar@hotmail.com>
2022-05-13 14:55:37 +0200

    Verse Bananenlevering
    Is niet meer kapoet

--- Practicum 1b/practicum-1bbb.py
+++ Practicum 1b/practicum-1bbb.py
@@ -1,6 +1,7 @@
 """
 Practicum Regetechniek van Daan Sijnja (20177747), Jeroen Vogelezang (20093179), Nils Bebelaar (20164882). Wij kunnen echt goed regelen.
 """
+from turtle import color
 import numpy as np
 import matplotlib.pylab as plt
 import control
@@ -12,18 +13,23 @@ H = control.tf(T, N)
 
 print(H)
 
-control.bode(H)  # alleen een plot
-mag, phase, omega = control.bode(H)  # een plot en nummerieke uitvoer
+#control.bode(H, omega=np.logspace(-1,5,300))  # alleen een plot
+mag, phase, omega = control.bode(H, omega=np.logspace(-1,5,300))  # een plot en nummerieke uitvoer
 
 x = [0, 1, 5, 10, 100]
-y = []
+y1 = []
+y2 = []
 
 for w in [0, 1, 5, 10, 100]:
     Hw = control.evalfr(H, 1j * w)
-    print(20 * np.log10(np.abs(Hw)))
-    y.append(np.abs(Hw))
+    y1.append(20 * np.log10(np.abs(Hw)))
+    y2.append(np.degrees(np.angle(Hw)))
 
-# control.singular_values_plot(H)
-# plt.scatter(x, y)
+all_axes = plt.gcf().get_axes()
+plt.sca(all_axes[0])
+plt.scatter(x, y1)
+
+plt.sca(all_axes[1])
+plt.scatter(x, y2)
 
 plt.show()
