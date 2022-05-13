1cd45edb0ac4301c1f74df00fb57a17314335dc1
nilsbebelaar <nils.bebelaar@hotmail.com>
2022-05-13 13:43:15 +0200

    Rotte bananenlevering
    Is kapoet

--- Practicum 1b/practicum-1bbb.py
+++ Practicum 1b/practicum-1bbb.py
@@ -8,21 +8,22 @@ import control.matlab as cm
 
 T = np.array([5.0])
 N = np.array([1.0, 5.0])
-H = control.tf(T,N)
+H = control.tf(T, N)
 
 print(H)
 
-control.nyquist(H)
-r = control.nyquist(H)
+control.bode(H)  # alleen een plot
+mag, phase, omega = control.bode(H)  # een plot en nummerieke uitvoer
 
-x = []
+x = [0, 1, 5, 10, 100]
 y = []
 
-for w in [0, 1, 5, 10, 100,500]:
+for w in [0, 1, 5, 10, 100]:
     Hw = control.evalfr(H, 1j * w)
-    print(Hw.real,Hw.imag)
-    x.append(Hw.real)
-    y.append(Hw.imag)
+    print(20 * np.log10(np.abs(Hw)))
+    y.append(np.abs(Hw))
 
-plt.scatter(x,y)
+# control.singular_values_plot(H)
+# plt.scatter(x, y)
+
 plt.show()
