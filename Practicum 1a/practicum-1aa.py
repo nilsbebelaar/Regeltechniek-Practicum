371ca66d317b2fc49a9468dc4e31d60bbf0d12a2
DaanSijnja <68638963+DaanSijnja@users.noreply.github.com>
2022-05-13 12:35:18 +0200

    Bananan 2

--- Practicum 1a/practicum-1aa.py
+++ Practicum 1a/practicum-1aa.py
@@ -1,7 +1,7 @@
 """
 Practicum Regetechniek van Daan Sijnja (20177747), Jeroen Vogelezang (20093179), Nils Bebelaar (20164882). Wij kunnen echt goed regelen.
 """
-from tkinter import Y
+import math as mth
 import numpy as np
 import matplotlib.pylab as plt 
 import control
@@ -12,6 +12,11 @@ def find_doorschot(yrange, eind_waarde):
     index = yrange.tolist().index(max(yrange))
     return doorschot, index*((11.5)/len(yrange))
 
+def static_error(y_range,eind_waarde):
+    StatError = 0
+    for i in range(5): 
+        StatError = StatError +  eind_waarde-y_range[-i]
+    return StatError/5
 
 teller = np.array([18.0]) 
 noemer = np.array([1.0, 1.2,36]) 
@@ -32,12 +37,15 @@ Hclosed = control.feedback(Sys1,Sys2)
 t_open, y_open = control.step_response(Sys1)  
 t_closed, y_closed = control.step_response(Hclosed) 
 
-print(y_open)
 
+d1, t1 = find_doorschot(y_open,50)
+d2, t2 = find_doorschot(y_closed,1)
+print("Doorschot Open","Waarde:",round(d1,2), "Tijdstip", round(t1,2))
+print("Static Error Open",static_error(y_open,50))
 
+print("Doorschot Open","Waarde:", round(d2,2), "Tijdstip", round(t2,2))
+print("Static Error Open",static_error(y_closed,1))
 
-print(find_doorschot(y_open,50))
-print(find_doorschot(y_closed,1))
 
 plt.plot(t_open,y_open, t_closed, y_closed)
 plt.ylabel('Stap reponsie')
