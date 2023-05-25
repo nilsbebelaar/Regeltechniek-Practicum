c626991e09ea372e31fb2c8c9806fc046fb392f2
Nils Bebelaar <nils@bebelaar.net>
2023-05-25 15:13:53 +0200

    Update practicum3-vraag8.py

--- Nieuw/Practicum 3/practicum3-vraag8.py
+++ Nieuw/Practicum 3/practicum3-vraag8.py
@@ -6,40 +6,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control
 
-# Definieer de overdrachtsfunctie G(s)
-num = [1]
-den = [1, 2, 1]
-H = control.tf(num, den)
-
-# Definieer de waarde van Kp
+# Definieer de waarde van Kp en Kd
 Kp = 100
+Kd = 24.86
+m = 1  # kg
 
-# Definieer de lusversterking L(s) = Kp * G(s)
-L = Kp * H
 
-# Definieer de overdrachtsfunctie van het gesloten lussysteem
+G = control.tf([1], [m, 0, 0])
+C = Kp
+
+L = G * C
+
 T = L / (1 + L)
+print(control.poles(T))
 
-mag, phase, omega = control.bode(T, omega=np.logspace(-0.5, 2.5, 300), dB=True)
+mag, phase, omega = control.bode(T, omega=np.logspace(0, 2, 4000), dB=True, label="$T(s)$")
 
 t = np.linspace(0, 3, 1000)  # definitie tijdsvector
 u5 = np.sin(5*t)  # zelf definiëren
-y5 = control.forced_response(T,t,u5)[1] 
+y5 = control.forced_response(T, t, u5)[1]
 u10 = np.sin(10*t)  # zelf definiëren
-y10 = control.forced_response(T,t,u10)[1] 
+y10 = control.forced_response(T, t, u10)[1]
 
 
-#bode plot
+# bode plot
 all_axes = plt.gcf().get_axes()
 plt.sca(all_axes[0])
+plt.title("Bode plot van $T(s)$")
 plt.legend()
 
-#maakt plot 2 aan
+# maakt plot 2 aan
 fig, ax = plt.subplots(1)
-ax.plot(t,u5, label=f"$\sin(5t)$")
-ax.plot(t,u10, label=f"$\sin(10t)$")
-plt.plot(t,y5, label=f"response to $\sin(5t)$")
-plt.plot(t,y10, label=f"response to $\sin(10t)$")
+ax.plot(t, u5, label=f"$\sin(5t)$")
+ax.plot(t, u10, label=f"$\sin(10t)$")
+plt.plot(t, y5, label=f"response to $\sin(5t)$")
+plt.plot(t, y10, label=f"response to $\sin(10t)$")
 plt.xlabel('Tijd [s]')
 plt.legend()
 plt.show()
